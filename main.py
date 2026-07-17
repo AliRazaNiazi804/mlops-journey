7ce790a4628b99877950401775ccd43f16e692d3
Ali Raza Khan Niazi <aliniazi8044@gmail.com>
2026-07-17 15:16:05 +0500

    added FastAPI endpoint for ML prediction

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+# create FastAPI object
+app = FastAPI()
+#yeh ek GET request hai
+@app.get("/")
+def read_root():
+    return{"message": "Mlops Server is Live!"}
+#yeh ek ML model jaisa dummy endpoint hai
+@app.get("/predict")
+def predict_score(hours_studied:int):
+    #yahan ML ka logic aayegam abhi simple math hai
+    score = hours_studied * 10
+    return {"Predicted Score": score}
